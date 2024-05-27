a9c6af9c2f9390a2735e51129e34c74679c75473
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-05-27 23:18:40 +0530

    import react-router-dom in index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import "./style.css"
 import Home from "./views/Home/Home"
 import About from "./views/About/About"
 import Contact from "./views/Contact/Contact"
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 
-root.render(<Home />)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+]);
+
+root.render(<RouterProvider router={router}/>)
+
